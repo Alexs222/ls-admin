e64e150a708fed2321cc1648e07b5ae03fba42ff
alexs222 <aleks.nikulin@gmail.com>
2018-05-08 22:01:05 +0200

    first commit

---
+++ src/components/todo.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+    .todo
+        //- pre {{todos}}
+        todo-input(
+            @addTodo="addTodo"
+        )
+        todo-list(
+            :todos="todos"
+            @removeTodo="removeTodo"
+            @checkTodo="checkTodo"
+        )
+</template>
+
+
+<script>
+import todoInput from "./todoInput";
+import todoList from "./todoList";
+
+// import {eventBus} from "../main.js";
+
+export default {
+    components: {
+        todoInput, todoList
+    },
+    data() {
+        return {
+            todos: []
+        }
+    },
+    methods: {
+        addTodo(todo) {
+            this.todos.push(todo);
+        },
+        removeTodo(todoId) {
+            this.todos = this.todos.filter(item => item.id !== todoId);
+        },
+        checkTodo(todo) {
+            this.todos = this.todos.map(item => item.id === todo.id ? todo : item);
+        }
+    }
+    // mounted() {
+        // eventBus.$on('removeTodo', todoId => {
+            // this.todos = this.todos.filter(item => item.id !== todoId);
+        // })
+    // }
+};
+</script>
+
+<style lang="scss" src="styles/todo.scss" scoped>
+
+</style>
+
+
---
+++ src/components/todoItem.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+    .todo-item
+        label.label(:class="{checked: todo.checked}")
+            .input-block
+                input(
+                    type="checkbox"
+                    @change="checkTodo"
+                    :checked="todo.checked" 
+                ).input
+            .title {{todo.name}}
+        .button
+            button(
+                type="button"
+                @click="removeTodo"
+            ).remove
+                | x
+</template>
+
+<script>
+// import {eventBus} from '../main.js'
+
+export default {
+    props: {
+        todo: Object
+    },
+    methods: {
+        removeTodo() {
+            // eventBus.$emit('removeTodo', this.todo.id)
+            this.$emit('removeTodo', this.todo.id);
+        },
+        checkTodo(e) {
+            const todoItem = {
+                ...this.todo,
+                checked: e.target.checked
+            }
+            this.$emit('checkTodo', todoItem)
+        }
+    }
+}
+</script>
+
+<style src="styles/todoItem.scss" lang="scss" scoped></style>
+
